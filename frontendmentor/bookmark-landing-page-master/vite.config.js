f277e5e6f36f7c6ee357fc7e21500814844417ac
Aymane <138299321+AymaneOnline@users.noreply.github.com>
2025-08-01 20:24:41 +0100

    updates

--- frontendmentor/bookmark-landing-page-master/vite.config.js
+++ frontendmentor/bookmark-landing-page-master/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: '/frontendmentor/space-tourism-website-main-deploy/', // 👈 REQUIRED for GitHub Pages
   plugins: [
     tailwindcss(),
   ],
-  base: './',
 })
