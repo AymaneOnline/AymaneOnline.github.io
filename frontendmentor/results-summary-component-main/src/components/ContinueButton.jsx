e0f3d088913b20d3f5aa250a07c852d4e9a3ede7
Aymane <138299321+AymaneOnline@users.noreply.github.com>
2025-08-07 00:07:15 +0100

    new challenge

---
+++ frontendmentor/results-summary-component-main/src/components/ContinueButton.jsx
@@ -0,0 +1,7 @@
+import styles from './ContinueButton.module.scss';
+
+export default function ContinueButton() {
+    return (
+        <button className={styles["continue-btn"]}>Continue</button>
+    );
+}
