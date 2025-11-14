2e66ec876aa066c16dc689d53fc30b76a25d5ba1
AymaneOnline <aymaneprowork@gmail.com>
2025-11-14 08:45:04 +0100

    deploy of countries rest api app

--- frontendmentor/rest-countries-api-with-color-theme-switcher-master/vite.config.js
+++ frontendmentor/rest-countries-api-with-color-theme-switcher-master/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'AymaneOnline.github.io/frontendmentor',
+  base: '/frontendmentor/rest-countries-api-with-color-theme-switcher-master-deploy/',
 })
