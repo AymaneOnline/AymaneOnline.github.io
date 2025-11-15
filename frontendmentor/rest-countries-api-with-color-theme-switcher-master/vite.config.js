9abd855fcfad5347d0b87d19c9479cdab9a74e3f
AymaneOnline <aymaneprowork@gmail.com>
2025-11-15 11:16:40 +0100

    fixing the base path

--- frontendmentor/rest-countries-api-with-color-theme-switcher-master/vite.config.js
+++ frontendmentor/rest-countries-api-with-color-theme-switcher-master/vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'AymaneOnline.github.io/frontendmentor/rest-countries-api-with-color-theme-switcher-master/', // GitHub Pages path
+  base: 'AymaneOnline.github.io/frontendmentor/rest-countries-api-with-color-theme-switcher-master-deploy/', // GitHub Pages path
 })
